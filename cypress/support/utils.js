a8b94fd0f90dbaf5bb8297bfc9af3a278d5d3ac9
MaryMoroz <morozova.mary@gmail.com>
2022-05-01 16:21:58 -0400

    delete unuseful code

--- cypress/support/utils.js
+++
@@ -1,6 +0,0 @@
-export const login = (email, password) => {
-    cy.get('#user').type(email);
-    cy.get('#login').click();
-    cy.get('#password').type(password);
-    cy.get('#login-submit').click();
-}
